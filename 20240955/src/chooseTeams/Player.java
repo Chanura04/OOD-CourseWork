e466144db922eb47b0833fe261520d3370b7204b
Chanura04 <167898990+Chanura04@users.noreply.github.com>
2025-10-12 00:41:54 +0530

    Created player data read function

--- 20240955/src/chooseTeams/Player.java
+++ 20240955/src/chooseTeams/Player.java
@@ -1,6 +1,14 @@
 package chooseTeams;
 
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Player extends Person {
     // Each player personal details
     private int totalScore;
@@ -65,6 +73,7 @@ public class Player extends Person {
         return "Player ID: " + id + "\n" + "Player Name: " + name + "\n" + "Player Email: " + email + "\n" + "Player Preferred Role: " + preferredRole + "\n" + "Player Personality Type: " + personalityType + "\n" + "Player Skill Level: " + skillLevel + "\n" + "Player Interest Sport: " + interestSport;
     }
 
+    // For get the personality type
     public int calculateTotalScore(int q1_ans, int q2_ans, int q3_ans, int q4_ans, int q5_ans){
         totalScore = (q1_ans + q2_ans + q3_ans + q4_ans + q5_ans) *4;
         setTotalScore(totalScore);
@@ -85,7 +94,41 @@ public class Player extends Person {
         else{
             return "Nan";
         }
+    }
 
+    //Store player data into a csv file
+    public void storePlayerData(){
+        //save to csv file
+        System.out.println("Player Data Stored Successfully");
+    }
+
+
+    public ArrayList<String> getPlayerData(){
+        String filePath = "./data/participants_sample.csv";
+        String line;
+        Map<String, String> player = new HashMap<>();
+
+        ArrayList<String> playerData = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                player.put("id", values[0]);
+                player.put("name", values[1]);
+                player.put("email", values[2]);
+                player.put("preferredGame", values[3]);
+                player.put("skillLevel", values[4]);
+                player.put("preferredRole", values[5]);
+                player.put("personalityScore", values[6]);
+                player.put("personalityType", values[7]);
+
+                playerData.add(player.toString());
+
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return playerData;
     }
 
 }
---
+++ 20240955/src/chooseTeams/TeamMembersSelection.java
@@ -0,0 +1,4 @@
+package chooseTeams;
+
+public class TeamMembersSelection {
+}
