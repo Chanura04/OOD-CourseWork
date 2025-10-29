d4e20ee74ac012770e840de65905dd465dd87bca
Chanura04 <167898990+Chanura04@users.noreply.github.com>
2025-10-29 22:13:18 +0530

    Fix the adding new players to csv file bug in storeNewPlayerData method

--- 20240955/src/chooseTeams/Player.java
+++ 20240955/src/chooseTeams/Player.java
@@ -1,14 +1,9 @@
 package chooseTeams;
 
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Player extends Person {
     // Each player personal details
@@ -20,13 +15,10 @@ public class Player extends Person {
     private String id;
 
     //Player class constructor
-    public Player( String name, String email, String preferredRole, String personalityType, int skillLevel, String interestSport) {
+    public Player( String name, String email) {
         super( name, email);
-        this.preferredRole = preferredRole;
+
         setId(newPlayerId());
-        this.personalityType = personalityType;
-        this.skillLevel = skillLevel;
-        this.interestSport = interestSport;
     }
 
 
@@ -36,14 +28,15 @@ public class Player extends Person {
     public String getId(){
         return id;
     }
-    @Override
-    public void setName(String name) {
-        this.name = name;
-    }
-    @Override
-    public void setEmail(String email) {
-        this.email = email;
-    }
+
+//    @Override
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//    @Override
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
 
     public String getPreferredRole(){
         return preferredRole;
@@ -75,50 +68,53 @@ public class Player extends Person {
     public void setTotalScore(int totalScore){
         this.totalScore = totalScore;
     }
+
     public String toString(){
-        return "Player ID: " + id + "\n" + "Player Name: " + name + "\n" + "Player Email: " + email + "\n" + "Player Preferred Role: " + preferredRole + "\n" + "Player Personality Type: " + personalityType + "\n" + "Player Skill Level: " + skillLevel + "\n" + "Player Interest Sport: " + interestSport;
+        return "Player ID: " + getId() + "\n" + "Player Name: " + getName() + "\n" + "Player Email: " + getEmail() + "\n" + "Player Preferred Role: " + getPreferredRole() + "\n" + "Player Personality Type: " + getPersonalityType() + "\n" + "Player Skill Level: " + skillLevel + "\n" + "Player Interest Sport: " + interestSport;
     }
 
     // For get the personality type
-    public int calculateTotalScore(int q1_ans, int q2_ans, int q3_ans, int q4_ans, int q5_ans){
+    public void calculateTotalScore(int q1_ans, int q2_ans, int q3_ans, int q4_ans, int q5_ans){
         totalScore = (q1_ans + q2_ans + q3_ans + q4_ans + q5_ans) *4;
         setTotalScore(totalScore);
-        return totalScore;
     }
 
-    public String checkPersonalityType(){
+    public boolean checkPersonalityType(){
         int score= getTotalScore();
         if(score>=90){
-            return "Leader";
+            setPersonalityType("Leader");
         }
         else if(score>=70){
-            return "Balanced";
+            setPersonalityType("Balanced");
         }
         else if(score>=50){
-            return "Thinker";
+            setPersonalityType("Thinker");
         }
         else{
-            return "Nan";
+            return false;
         }
+        return true;
     }
 
     //Store player data into a csv file
     public void storeNewPlayerData(){
         //save to csv file
-        try (FileWriter writer = new FileWriter("students_loop.csv", true)) {
-            String[] data={
-                    getId(),getName(),getEmail(),getInterestSport(), String.valueOf(getSkillLevel()),getPreferredRole(), String.valueOf(getTotalScore()),getPersonalityType()
-            };
-            writer.append(String.join(",", data));
-            writer.append("\n");
+        if(checkPersonalityType()){
+            try (FileWriter writer = new FileWriter("students_loop.csv", true)) {
+                String[] data={
+                        getId(),getName(),getEmail(),getInterestSport(), String.valueOf(getSkillLevel()),getPreferredRole(), String.valueOf(getTotalScore()),getPersonalityType()
+                };
+                writer.append(String.join(",", data));
+                writer.append("\n");
 
-
-
-
-            System.out.printf("%s successfully added!",getName());
-        } catch (IOException e) {
-            e.printStackTrace();
+                System.out.printf("%s successfully added!",getName());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else{
+            System.out.println("Your Personality Score is very low!!!. please try again!");
         }
+
 
     }
 
@@ -147,33 +143,11 @@ public class Player extends Person {
         return "P"+(previousId + 1);
     }
 
-//    public  ArrayList<String> getPlayerData(){
-////        String filePath = "./data/participants_sample.csv";
-//        String filePath = "students_loop.csv";
-//        String line;
-//        Map<String, String> player = new HashMap<>();
+//    @Override
+//    public String toString(){
+//        return "Player ID: " + id+"\n" + "Player Name: " + name+"\n"+"Email: "+email;
 //
-//        ArrayList<String> playerData = new ArrayList<>();
-//
-//        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-//            while ((line = br.readLine()) != null) {
-//                String[] values = line.split(",");
-//                player.put("id", values[0]);
-//                player.put("name", values[1]);
-//                player.put("email", values[2]);
-//                player.put("preferredGame", values[3]);
-//                player.put("skillLevel", values[4]);
-//                player.put("preferredRole", values[5]);
-//                player.put("personalityScore", values[6]);
-//                player.put("personalityType", values[7]);
-//
-//                playerData.add(player.toString());
-//
-//            }
-//        } catch (IOException e) {
-//            System.out.println(e.getMessage());
-//        }
-//        return playerData;
 //    }
+
 
 }
