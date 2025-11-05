04af85e14e91bb5e223690b2071f55f4dbcc7b4d
Chanura04 <167898990+Chanura04@users.noreply.github.com>
2025-11-05 19:43:22 +0530

    Use method overloading for getPlayerData method.
    When start the program initially program access to the inside the data folder csv participant data base, that csv file pass as File object and creating teams after upload required data file it save in same directory and that file simply pass from the file name.

--- 20240955/src/chooseTeams/PlayerDataLoader.java
+++ 20240955/src/chooseTeams/PlayerDataLoader.java
@@ -1,9 +1,6 @@
 package chooseTeams;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -11,6 +8,36 @@ import java.util.Set;
 
 
 public class PlayerDataLoader {
+
+    public ArrayList<String> getPlayerData(File csvFilePath){
+//        String filePath = "data/participants_sample.csv";
+
+        String line;
+        String[] player=new String[8];
+
+        ArrayList<String> playerData = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFilePath))) {
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                player[0]=values[0];
+                player[1]=values[1];
+                player[2]=values[2];
+                player[3]=values[3];
+                player[4]=values[4];
+                player[5]=values[5];
+                player[6]=values[6];
+                player[7]=values[7];
+
+                playerData.add(Arrays.toString(player));
+
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return playerData;
+    }
     public ArrayList<String> getPlayerData(String csvFilePath){
 //        String filePath = "data/participants_sample.csv";
 
